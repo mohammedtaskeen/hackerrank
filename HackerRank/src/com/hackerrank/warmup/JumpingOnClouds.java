3607564583ddd04bbf736eebee7f51bab7c80831
mohammedtaskeen <mohammedtaskeen@gmail.com>
2019-04-15 15:04:25 -0500

    Added warm up solutions

---
+++ HackerRank/src/com/hackerrank/warmup/JumpingOnClouds.java
@@ -0,0 +1,58 @@
+package com.hackerrank.warmup;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class JumpingOnClouds {
+
+/**
+ 	100
+	0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 1 0 0 0 0 1 0 0 1 0 0 1 0 1 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 0 1 0 1 0 1 0 1 0 0 0 0 0 0 1 0 0 0
+
+	53
+
+ */
+	
+	static int jumpingOnClouds(int[] c) {
+    	int noOfJumps = 0,i=0;
+    	while(i<c.length-1) {
+    		if(c[i+1]==1||(i+2<c.length && c[i+2]==0)) {
+    			i=i+2;
+    		} else {
+    			i=i+1;
+    		}
+    		noOfJumps++;
+    	}
+    	
+		return noOfJumps;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("HACKERRANK_OUTPUT")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ HackerRank/src/com/hackerrank/warmup/SockMerchant.java
@@ -0,0 +1,63 @@
+package com.hackerrank.warmup;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    @SuppressWarnings("rawtypes")
+	static int sockMerchant(int n, int[] ar) {
+    	
+    	HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        for(int i: ar) {
+            if(hmap.containsKey(Integer.valueOf(i))) {
+                hmap.put(Integer.valueOf(i), hmap.get(Integer.valueOf(i))+1);
+            } else {
+                hmap.put(Integer.valueOf(i),1);
+            }
+        }
+        int noOfPairs = 0;
+        Set set = hmap.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry)iterator.next();
+            noOfPairs += (Integer) mentry.getValue()/2;
+        }
+        return noOfPairs;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        System.out.println(result);
+        scanner.close();
+    }
+}
